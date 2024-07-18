6550432a6ccfeeaa38c5f1f41440dce924a1e35e
uygaronder <uonder50@gmail.com>
2024-07-18 16:46:16 +0300

    chore: Refactor nodemailer.js and remove console.log statements

--- app/utils/nodemailer.js
+++ app/utils/nodemailer.js
@@ -2,8 +2,6 @@ const nodemailer = require('nodemailer');
 
 const defMailTemplate = "./mailTemplates/defaultdefMailTemplate";
 
-console.log(defMailTemplate);
-
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -13,17 +11,7 @@ const transporter = nodemailer.createTransport({
 });
 
 async function sendMail(email, subject, template = defMailTemplate, data = {}) {
-    try {
-        await transporter.sendMail({
-            from: process.env.EMAIL,
-            to: email,
-            subject,
-            text
-        });
-        console.log('Email sent');
-    } catch (error) {
-        console.error('Error sending email');
-    }
+    // handle sendmail
 }
 
 module.exports = transporter;
